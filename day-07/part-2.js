db578e21a12a8d166ccfa6f6dfebd6b85eecf326
“Keith <keith.carter@yahoo.com>
2024-12-06 21:57:03 -0800

    day 7

---
+++ day-07/part-2.js
@@ -0,0 +1,44 @@
+function evaluate(testValue, currentValue, numbers) {
+    if (numbers.length == 1) {
+        return (
+            currentValue * numbers[0] == testValue) ||
+            (currentValue + numbers[0] == testValue) ||
+            parseInt(currentValue.toString() + numbers[0].toString()) == testValue
+    } else {
+        return evaluate(
+            testValue,
+            currentValue * numbers[0],
+            numbers.slice(1) 
+        ) || evaluate(
+            testValue,
+            currentValue + numbers[0],
+            numbers.slice(1) 
+        ) || evaluate(
+            testValue,
+            parseInt(currentValue.toString() + numbers[0].toString()),
+            numbers.slice(1) 
+        )
+    }
+}
+
+const readline = require('readline')
+const fs = require('fs')
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('./day-07/input'),
+})
+
+let sum = 0
+readInterface.on('line', function(line) {
+    let numbers = line.split(': ')[1].split(' ').map(item => parseInt(item))
+    let testValue = parseInt(line.split(': ')[0])
+    if (evaluate(
+        testValue,
+        numbers[0],
+        numbers.slice(1)
+    )) {
+        sum += testValue
+    }   
+}).on('close', function() {
+    console.log(sum)
+})
